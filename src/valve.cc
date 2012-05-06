84359af2d0665b62d0d7f3d4473319f597524b82
Francesco Salvestrini <salvestrini@gmail.com>
2012-05-06 22:13:05 +0200

    Fixed options handling

--- src/valve.cc
+++ src/valve.cc
@@ -106,7 +106,7 @@ int main(int argc, char * argv[])
 
                 opterr = 0;
                 while ((opt = getopt_long(argc, argv,
-                                          "hVv",
+                                          "hVvd",
                                           long_options, &opt_index)) != -1) {
                         switch (opt) {
                                 case 'h':
