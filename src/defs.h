bb39dbbbfc98c5b3f9bb9f830b7c27881755344c
Francesco Salvestrini <salvestrini@gmail.com>
2012-05-06 17:20:29 +0200

    Added header guard

---
+++ src/defs.h
@@ -0,0 +1,12 @@
+//
+// This file is part of the valve package
+//
+// (C) 2012 Francesco Salvestrini <salvestrini AT gmail DOT com>
+//
+
+#ifndef VALVE_DEFS_H
+#define VALVE_DEFS_H
+
+#define PROGRAM_NAME "valve"
+
+#endif
