aad1d7aae4250b5a00d0078d525ab0f47fa32632
Francesco Salvestrini <salvestrini@gmail.com>
2012-05-06 17:31:12 +0200

    Added option parsing (using getopt_long)

--- src/valve.cc
+++ src/valve.cc
@@ -4,13 +4,92 @@
 // (C) 2012 Francesco Salvestrini <salvestrini AT gmail DOT com>
 //
 
-#include <iostream>
-
 #include "config.h"
+
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+
+#include <getopt.h>
+
+#include "defs.h"
+#include "utils.h"
+
+static std::string version()
+{
+        std::stringstream ss;
+        ss << PACKAGE_NAME << " " << PACKAGE_VERSION;
+        return ss.str();
+}
+
+static void hint(const std::string & message)
+{
+        std::cerr << PROGRAM_NAME << ": " << message << std::endl;
+        std::cerr << "Try `" << PROGRAM_NAME << " --help' for "
+                  << "more information." << std::endl;
+}
+
+static void help()
+{
+        std::cout << "Usage: " << PROGRAM_NAME << " [OPTION].. [FILE]..."
+                  << std::endl;
+        std::cout << std::endl;
+        std::cout << "  -h, --help       print this help, then exit"
+                  << std::endl;
+        std::cout << "  -V, --version    print version, then exit"
+                  << std::endl;
+        std::cout << std::endl;
+        std::cout << "Report bugs to <" << PACKAGE_BUGREPORT << ">"
+                  << std::endl;
+}
 
 int main(int argc, char * argv[])
 {
-        std::cout << PACKAGE_NAME << " " << PACKAGE_VERSION << std::endl;
+        try {
+                int opt       = 0;
+                int opt_index = 0;
 
-        return 0;
+                struct option long_options[] = {
+                        {"help",    no_argument, 0, 'h'},
+                        {"version", no_argument, 0, 'V'},
+                        {0,         0,           0, 0}
+                };
+
+                opterr = 0;
+                while ((opt = getopt_long(argc, argv,
+                                          "hV", long_options,
+                                          &opt_index)) != -1) {
+                        switch (opt) {
+                                case 'h':
+                                        help();
+                                        return EXIT_SUCCESS;
+                                case 'V':
+                                        std::cout << version() << std::endl;
+                                        return EXIT_SUCCESS;
+                                default:
+                                        hint("unknown argument");
+                                        return EXIT_FAILURE;
+                        }
+                }
+
+                if (opt_index >= argc) {
+                        hint("missing filename");
+                        return EXIT_FAILURE;
+                }
+
+                while (optind < argc) {
+                        std::string filename(argv[optind++]);
+                        std::cout << "handling "
+                                  << "`" << filename << "'"
+                                  << std::endl;
+                }
+        } catch (std::exception & e) {
+                std::stringstream ss;
+                ss << "Got unhandled exception (" << e.what() << ")";
+                bug(ss.str());
+        } catch (...) {
+                bug("Got unhandled exception");
+        }
+
+        return EXIT_SUCCESS;
 }
