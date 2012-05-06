3582e99534d8f9a8e56759a54cec1ae4a48882ac
Francesco Salvestrini <salvestrini@gmail.com>
2012-05-06 16:34:11 +0200

    Added main program generation, fixed repository related files (.gitignore)

---
+++ src/valve.cc
@@ -0,0 +1,16 @@
+//
+// This file is part of the valve package
+//
+// (C) 2012 Francesco Salvestrini <salvestrini AT gmail DOT com>
+//
+
+#include <iostream>
+
+#include "config.h"
+
+int main(int argc, char * argv[])
+{
+        std::cout << PACKAGE_NAME << " " << PACKAGE_VERSION << std::endl;
+
+        return 0;
+}
